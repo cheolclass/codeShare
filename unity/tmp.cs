7a3daade01734e1b9327b6d39c63341a22cbc652
cheolclass <cheolclass@gmail.com>
2025-07-23 15:16:53 +0900

    Update tmp.cs

--- unity/tmp.cs
+++ unity/tmp.cs
@@ -1,1 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameMain : MonoBehaviour
+{
+    private AudioMngr audio;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        audio = GetComponent<AudioMngr>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void OnClickPlayBGM()
+    {
+        audio.PlayNextBGM();
+    }
+
+    public void PlayBGM(int idx)
+    {
+        audio.PlayBGM(idx);
+    }
+
+    public void PlaySound(int idx)
+    {
+        audio.PlaySound(idx);
+    }
+}
 
