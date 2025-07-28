0c28f7c2b052090892e69b26a607e4dd1f2c9a6b
cheolclass <cheolclass@gmail.com>
2025-07-28 06:18:17 +0000

    2

--- C/main.c
+++ C/main.c
@@ -2,12 +2,11 @@
 
 int main(void)
 {
-
     // comments
-    for(int i=0; i<100; i++)      
-        for(int j=0; j<100; j++)
+    for (int i = 0; i < 100; i++)
+        for (int j = 0; j < 100; j++)
             printf("%d x %d = %.0lf \n",
-                i, j, i*j);
+                   i, j, i * j);
 
     return 0;
 }
