f16fc7a1da1debfecc99887c6b5e40a914d0468b
cheolclass <cheolclass@gmail.com>
2025-07-30 05:04:40 +0000

    1

--- unity/scripts/Clock.cs
+++ unity/scripts/Clock.cs
@@ -14,32 +14,33 @@ using UnityEngine;
 	// "#11dea1ff"	
 */
 
-public class Clock : MonoBehaviour { 
-	
+public class Clock : MonoBehaviour
+{
+
 	private const float
-	hoursToDegrees = -360f / 12f,	// 시/분/초 침의 회전 방향 => 시계 방향(음(-)의 방향)
+	hoursToDegrees = -360f / 12f,   // 시/분/초 침의 회전 방향 => 시계 방향(음(-)의 방향)
 	minutesToDegrees = -360f / 60f,
 	secondsToDegrees = -360f / 60f,
-	
-	speed = 3.0f,	// swing speed
-	swing = 20.0f,	// swing angle
+
+	speed = 3.0f,   // swing speed
+	swing = 20.0f,  // swing angle
 
 	door_time = 0.8f,
 	door_angle = 160f,
-	door_speed = door_angle/door_time,
+	door_speed = door_angle / door_time,
 
 	birdmove_time = 0.2f,
 	birdmove_distance = 0.5f,
-	birdmove_speed = birdmove_distance/birdmove_time;
+	birdmove_speed = birdmove_distance / birdmove_time;
 
 	public Transform hours, minutes, seconds, pendlum;
 	public Transform door_left, door_right, bird;
 
 	public AudioSource SoundCuckoo;
-    public AudioSource SoundOpen;
-    public AudioSource SoundClose;
+	public AudioSource SoundOpen;
+	public AudioSource SoundClose;
 
-	public bool analog;
+	public bool analog; // 에디터에서 설정 
 	public bool trigger;
 
 	// bird movement
@@ -62,50 +63,57 @@ public class Clock : MonoBehaviour {
 		trigger = false;
 	}
 
-	void Update () {
+	void Update()
+	{
 		float ang;
 		DateTime time = DateTime.Now;
-		TimeSpan timespan = DateTime.Now.TimeOfDay;
-		
-		ang = -90.0f+(float)timespan.TotalHours * hoursToDegrees; // >> 시/분/초 침의 회전 방향 => 시계 방향(음(-)의 방향)
-		hours.localRotation = Quaternion.Euler(ang, 0f, 0f);
-		
-		ang = -90.0f+(float)timespan.TotalMinutes * minutesToDegrees;
+		TimeSpan timespan = DateTime.Now.TimeOfDay; // time vs. timespan 차이 
+
+		ang = -90.0f + (float)timespan.TotalHours * hoursToDegrees; // >> 시/분/초 침의 회전 방향 => 시계 방향(음(-)의 방향)
+		hours.localRotation = Quaternion.Euler(ang, 0f, 0f);    // 사원수 
+
+		ang = -90.0f + (float)timespan.TotalMinutes * minutesToDegrees;
 		minutes.localRotation = Quaternion.Euler(ang, 0f, 0f);
 
-		if (analog) {
-			ang = -90.0f+(float)timespan.TotalSeconds * secondsToDegrees;
+		if (analog) // 침의 움직임이 더 부드럽다
+		{
+			ang = -90.0f + (float)timespan.TotalSeconds * secondsToDegrees;
 			seconds.localRotation = Quaternion.Euler(ang, 0f, 0f);
 		}
-		else {
-			ang = -90.0f + time.Second * secondsToDegrees;
-            		seconds.localRotation = Quaternion.Euler(ang, 0f, 0f);
-        	}
+		else
+		{
+			ang = -90.0f + time.Second * secondsToDegrees; //B. DateTime time 
+			seconds.localRotation = Quaternion.Euler(ang, 0f, 0f);
+		}
 
 		// pendlum control
-		if (time.Second < last_sec || trigger) {
+		if (time.Second < last_sec || trigger)
+		{
 			StartCoroutine(PlayCuckoo());
 			trigger = false;
 		}
 		last_sec = time.Second;
 
 		t += Time.deltaTime * speed;
-		ang = -90.0f + Mathf.Sin (t) * swing;
+		ang = -90.0f + Mathf.Sin(t) * swing;
 		pendlum.localRotation = Quaternion.Euler(ang, 0f, 0f);
 	}
 
-	IEnumerator PlayCuckoo() {
+	IEnumerator PlayCuckoo()
+	{
 		float t;
 		Vector3 door_left_rot = door_left.rotation.eulerAngles;
 		Vector3 door_right_rot = door_right.rotation.eulerAngles;
 
-		if (!bird_active) {
-			bird_active = true;	
+		if (!bird_active)
+		{
+			bird_active = true;
 
-			SoundOpen.Play ();
+			SoundOpen.Play();
 			// 1. open door in 1 sec
 			t = 0.0f;
-			while (t < door_time) {
+			while (t < door_time)
+			{
 				t += Time.deltaTime;
 				door_left.Rotate(
 					new Vector3(0f, 0f, Time.deltaTime * door_speed));
@@ -113,15 +121,16 @@ public class Clock : MonoBehaviour {
 					new Vector3(0f, 0f, -Time.deltaTime * door_speed));
 				yield return null;
 			}
-			door_left.rotation = 
-				Quaternion.Euler(door_left_rot + new Vector3 (0f, 0f, door_angle));
-			door_right.rotation = 
-				Quaternion.Euler(door_right_rot + new Vector3 (0f, 0f, -door_angle));
+			door_left.rotation =
+				Quaternion.Euler(door_left_rot + new Vector3(0f, 0f, door_angle));
+			door_right.rotation =
+				Quaternion.Euler(door_right_rot + new Vector3(0f, 0f, -door_angle));
 			yield return new WaitForSeconds(0.3f);
 
 			// 2. move bird out
 			t = 0.0f;
-			while (t < birdmove_time) {
+			while (t < birdmove_time)
+			{
 				t += Time.deltaTime;
 				bird.Translate(
 					new Vector3(-Time.deltaTime * birdmove_speed, 0f, 0f));
@@ -131,40 +140,46 @@ public class Clock : MonoBehaviour {
 
 			// bird sing multiple
 			int count = DateTime.Now.Hour;
-			if (count == 0) {
+			if (count == 0)
+			{
 				count = 12;
 			}
-			if (count > 12) {
+			if (count > 12)
+			{
 				count -= 12;
 			}
 
-			for (int i = 0; i < count; i++) {
-		                // 3-1. bird swing
-		                SoundCuckoo.Play();
-		
-		                t = 0.0f;
-				while (t < 0.4f) {
+			for (int i = 0; i < count; i++)
+			{
+				// 3-1. bird swing
+				SoundCuckoo.Play();
+
+				t = 0.0f;
+				while (t < 0.4f)
+				{
 					t += Time.deltaTime;
 					bird.Rotate(
-						new Vector3 (0f, -Time.deltaTime * 80.0f, 0f));
+						new Vector3(0f, -Time.deltaTime * 80.0f, 0f));
 					yield return null;
 				}
 
 				// 3-2. bird swing back
 				t = 0.0f;
-				while (t < 0.4f) {
+				while (t < 0.4f)
+				{
 					t += Time.deltaTime;
 					bird.Rotate(
-						new Vector3 (0f, Time.deltaTime * 80.0f, 0f));
+						new Vector3(0f, Time.deltaTime * 80.0f, 0f));
 					yield return null;
 				}
 
-				yield return new WaitForSeconds (0.3f);
+				yield return new WaitForSeconds(0.3f);
 			}
 
 			// 4. move bird back
 			t = 0.0f;
-			while (t < birdmove_time) {
+			while (t < birdmove_time)
+			{
 				t += Time.deltaTime;
 				bird.Translate(
 					new Vector3(Time.deltaTime * birdmove_speed, 0f, 0f));
@@ -173,10 +188,11 @@ public class Clock : MonoBehaviour {
 
 			yield return new WaitForSeconds(0.3f);
 
-            // Final phase, close door
-            SoundClose.Play();
-            t = 0.0f;
-			while (t < door_time) {
+			// Final phase, close door
+			SoundClose.Play();
+			t = 0.0f;
+			while (t < door_time)
+			{
 				t += Time.deltaTime;
 				door_left.Rotate(
 					new Vector3(0f, 0f, -Time.deltaTime * door_speed));
