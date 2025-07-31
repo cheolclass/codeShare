188811afa853cd00602785adec874bfabe1d07a1
cheolclass <cheolclass@gmail.com>
2025-07-31 19:21:16 +0900

    Update Clock.cs

--- unity/scripts/Clock.cs
+++ unity/scripts/Clock.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections;
 using UnityEngine;
 
@@ -17,51 +17,51 @@ using UnityEngine;
 public class Clock : MonoBehaviour
 {
 
-	private const float
-	hoursToDegrees = -360f / 12f,   // 시/분/초 침의 회전 방향 => 시계 방향(음(-)의 방향)
-	minutesToDegrees = -360f / 60f,
-	secondsToDegrees = -360f / 60f,
+    private const float
+    hoursToDegrees = -360f / 12f,   // 시/분/초 침의 회전 방향 => 시계 방향(음(-)의 방향)
+    minutesToDegrees = -360f / 60f,
+    secondsToDegrees = -360f / 60f,
 
-	speed = 3.0f,   // swing speed
-	swing = 20.0f,  // swing angle
 
-	door_time = 0.8f,
-	door_angle = 160f,
-	door_speed = door_angle / door_time,
+    speed = 3.0f,   // swing speed
+    swing = 20.0f,  // swing angle
+    door_time = 0.8f,
+    door_angle = 160f,
+    door_speed = door_angle / door_time,
 
-	birdmove_time = 0.2f,
-	birdmove_distance = 0.5f,
-	birdmove_speed = birdmove_distance / birdmove_time;
+    birdmove_time = 0.2f,
+    birdmove_distance = 0.5f,
+    birdmove_speed = birdmove_distance / birdmove_time;
 
-	public Transform hours, minutes, seconds, pendlum;
-	public Transform door_left, door_right, bird;
+    public Transform hours, minutes, seconds, pendlum;
+    public Transform door_left, door_right, bird;
 
-	public AudioSource SoundCuckoo;
-	public AudioSource SoundOpen;
-	public AudioSource SoundClose;
+    public AudioSource SoundCuckoo;
+    public AudioSource SoundOpen;
+    public AudioSource SoundClose;
 
-	public bool analog; // 에디터에서 설정 
-	public bool trigger;
+    public bool analog; // 에디터에서 설정 
+    public bool trigger;
 
-	// bird movement
-	private bool bird_active;
-	private int bird_pahse;
+    // bird movement
+    private bool bird_active;
+    private int bird_pahse;
 
-	// pendlum & cuckoo 
-	private float t;
-	private float last_sec;
+    // pendlum & cuckoo 
+    private float t;
+    private float last_sec;
 
-	void Start()
-	{
-		t = 0.0f;
-		// cuckoo.Play();
+    void Start()
+    {
+        t = 0.0f;
+        // cuckoo.Play();
 
-		last_sec = 0.0f;
-		bird_active = false;
-		StartCoroutine(PlayCuckoo()); // 시작시, 일단 한 번 보여주고...
+        last_sec = 0.0f;
+        bird_active = false;
+        StartCoroutine(PlayCuckoo()); // 시작시, 뻐꾸기 일단 한 번 보여주고...
 
-		trigger = false;
-	}
+        trigger = false;
+    }
 
 	void Update()
 	{
@@ -69,9 +69,13 @@ public class Clock : MonoBehaviour
 		DateTime time = DateTime.Now;
 		TimeSpan timespan = DateTime.Now.TimeOfDay; // time vs. timespan 차이 
 
-		ang = -90.0f + (float)timespan.TotalHours * hoursToDegrees; // >> 시/분/초 침의 회전 방향 => 시계 방향(음(-)의 방향)
-		hours.localRotation = Quaternion.Euler(ang, 0f, 0f);    // 시, 사원수, Euler angle의  Gimbal-lock 문제
-																// 자신의 Pivot(자신의 원점)을 기준으로 시침을 회전
+		ang = -90.0f + (float)timespan.TotalHours * hoursToDegrees;
+			// 시/분/초 침의 회전 방향 => 시계 방향(음(-)의 방향).
+			// 참고) //Y. https://docs.google.com/document/d/1uYqFAUUjCjNlp_FarQmD0w9HNfjt4zXerDZbTLshs4Q/edit?tab=t.0#heading=h.ltuj4acvwlwr 
+
+		hours.localRotation = Quaternion.Euler(ang, 0f, 0f);
+			// 시(hour), 사원수, Euler angle의  //B. Gimbal-lock 문제
+			// 자신의 Pivot(자신의 원점)을 기준으로 시침을 회전
 
 		ang = -90.0f + (float)timespan.TotalMinutes * minutesToDegrees;
 		minutes.localRotation = Quaternion.Euler(ang, 0f, 0f);  // 분
@@ -87,8 +91,10 @@ public class Clock : MonoBehaviour
 			seconds.localRotation = Quaternion.Euler(ang, 0f, 0f);  // 한 번에 6도씩 회전하는 초침
 		}
 
-		if (time.Second < last_sec || trigger)  //G. trigger = true; 설정 => 오직 InspV에서.
-		{                                       //G.  time.Second < last_sec 이경우는 초침이 59 => 0으로 넘어갈때. 즉 매 정각에 뻐꾸기 등장
+		if (time.Second < last_sec || trigger)
+			// time.Second < last_sec 이경우는 초침이 59 => 0으로 넘어갈때. 즉 매 정각에 뻐꾸기 등장
+			// trigger = true; 설정 => 오직 InspV에서.
+		{
 			StartCoroutine(PlayCuckoo());
 			trigger = false;
 		}
@@ -97,114 +103,116 @@ public class Clock : MonoBehaviour
 		// pendlum control
 		t += Time.deltaTime * speed;
 		ang = -90.0f + Mathf.Sin(t) * swing;
-		pendlum.localRotation = Quaternion.Euler(ang, 0f, 0f);	// 자신의 Pivot(자신의 원점, 윗쪽 끝)을 기준으로 시계추를 회전
+		// 
+		pendlum.localRotation = Quaternion.Euler(ang, 0f, 0f);
+			// 자신의 Pivot(자신의 원점, 윗쪽 끝)을 기준으로 시계추를 회전
 	}
 
-	IEnumerator PlayCuckoo()
-	{
-		float t;
-		Vector3 door_left_rot = door_left.rotation.eulerAngles;
-		Vector3 door_right_rot = door_right.rotation.eulerAngles;
+    IEnumerator PlayCuckoo()
+    {
+        float t;
+        Vector3 door_left_rot = door_left.rotation.eulerAngles;
+        Vector3 door_right_rot = door_right.rotation.eulerAngles;
 
-		if (!bird_active)
-		{
-			bird_active = true;
+        if (!bird_active)
+        {
+            bird_active = true;
 
-			SoundOpen.Play();
-			// 1. open door in 1 sec
-			t = 0.0f;
-			while (t < door_time)
-			{
-				t += Time.deltaTime;
-				door_left.Rotate(
-					new Vector3(0f, 0f, Time.deltaTime * door_speed));
-				door_right.Rotate(
-					new Vector3(0f, 0f, -Time.deltaTime * door_speed));
-				yield return null;
-			}
-			door_left.rotation =
-				Quaternion.Euler(door_left_rot + new Vector3(0f, 0f, door_angle));
-			door_right.rotation =
-				Quaternion.Euler(door_right_rot + new Vector3(0f, 0f, -door_angle));
-			yield return new WaitForSeconds(0.3f);
+            SoundOpen.Play();
+            // 1. open door in 1 sec
+            t = 0.0f;
+            while (t < door_time)
+            {
+                t += Time.deltaTime;
+                door_left.Rotate(
+                    new Vector3(0f, 0f, Time.deltaTime * door_speed));
+                door_right.Rotate(
+                    new Vector3(0f, 0f, -Time.deltaTime * door_speed));
+                yield return null;
+            }
+            door_left.rotation =
+                Quaternion.Euler(door_left_rot + new Vector3(0f, 0f, door_angle));
+            door_right.rotation =
+                Quaternion.Euler(door_right_rot + new Vector3(0f, 0f, -door_angle));
+            yield return new WaitForSeconds(0.3f);
 
-			// 2. move bird out
-			t = 0.0f;
-			while (t < birdmove_time)
-			{
-				t += Time.deltaTime;
-				bird.Translate(
-					new Vector3(-Time.deltaTime * birdmove_speed, 0f, 0f));
-				yield return null;
-			}
-			yield return new WaitForSeconds(0.3f);
+            // 2. move bird out
+            t = 0.0f;
+            while (t < birdmove_time)
+            {
+                t += Time.deltaTime;
+                bird.Translate(
+                    new Vector3(-Time.deltaTime * birdmove_speed, 0f, 0f));
+                yield return null;
+            }
+            yield return new WaitForSeconds(0.3f);
 
-			// bird sing multiple
-			int count = DateTime.Now.Hour;
-			if (count == 0)
-			{
-				count = 12;
-			}
-			if (count > 12)
-			{
-				count -= 12;
-			}
+            // bird sing multiple
+            int count = DateTime.Now.Hour;
+            if (count == 0)
+            {
+                count = 12;
+            }
+            if (count > 12)
+            {
+                count -= 12;
+            }
 
-			for (int i = 0; i < count; i++)
-			{
-				// 3-1. bird swing
-				SoundCuckoo.Play();
+            for (int i = 0; i < count; i++)
+            {
+                // 3-1. bird swing
+                SoundCuckoo.Play();
 
-				t = 0.0f;
-				while (t < 0.4f)
-				{
-					t += Time.deltaTime;
-					bird.Rotate(
-						new Vector3(0f, -Time.deltaTime * 80.0f, 0f));
-					yield return null;
-				}
+                t = 0.0f;
+                while (t < 0.4f)
+                {
+                    t += Time.deltaTime;
+                    bird.Rotate(
+                        new Vector3(0f, -Time.deltaTime * 80.0f, 0f));
+                    yield return null;
+                }
 
-				// 3-2. bird swing back
-				t = 0.0f;
-				while (t < 0.4f)
-				{
-					t += Time.deltaTime;
-					bird.Rotate(
-						new Vector3(0f, Time.deltaTime * 80.0f, 0f));
-					yield return null;
-				}
+                // 3-2. bird swing back
+                t = 0.0f;
+                while (t < 0.4f)
+                {
+                    t += Time.deltaTime;
+                    bird.Rotate(
+                        new Vector3(0f, Time.deltaTime * 80.0f, 0f));
+                    yield return null;
+                }
 
-				yield return new WaitForSeconds(0.3f);
-			}
+                yield return new WaitForSeconds(0.3f);
+            }
 
-			// 4. move bird back
-			t = 0.0f;
-			while (t < birdmove_time)
-			{
-				t += Time.deltaTime;
-				bird.Translate(
-					new Vector3(Time.deltaTime * birdmove_speed, 0f, 0f));
-				yield return null;
-			}
+            // 4. move bird back
+            t = 0.0f;
+            while (t < birdmove_time)
+            {
+                t += Time.deltaTime;
+                bird.Translate(
+                    new Vector3(Time.deltaTime * birdmove_speed, 0f, 0f));
+                yield return null;
+            }
 
-			yield return new WaitForSeconds(0.3f);
+            yield return new WaitForSeconds(0.3f);
 
-			// Final phase, close door
-			SoundClose.Play();
-			t = 0.0f;
-			while (t < door_time)
-			{
-				t += Time.deltaTime;
-				door_left.Rotate(
-					new Vector3(0f, 0f, -Time.deltaTime * door_speed));
-				door_right.Rotate(
-					new Vector3(0f, 0f, Time.deltaTime * door_speed));
-				yield return null;
-			}
-			door_left.rotation = Quaternion.Euler(door_left_rot);
-			door_right.rotation = Quaternion.Euler(door_right_rot);
+            // Final phase, close door
+            SoundClose.Play();
+            t = 0.0f;
+            while (t < door_time)
+            {
+                t += Time.deltaTime;
+                door_left.Rotate(
+                    new Vector3(0f, 0f, -Time.deltaTime * door_speed));
+                door_right.Rotate(
+                    new Vector3(0f, 0f, Time.deltaTime * door_speed));
+                yield return null;
+            }
+            door_left.rotation = Quaternion.Euler(door_left_rot);
+            door_right.rotation = Quaternion.Euler(door_right_rot);
 
-			bird_active = false;
-		}
-	}
+            bird_active = false;
+        }
+    }
 }
