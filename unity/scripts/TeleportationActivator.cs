6357a0208604571038eed34606e2de4335006097
cheolclass <cheolclass@gmail.com>
2025-12-11 17:12:31 +0900

    Add TeleportationActivator class for teleport functionality
    Implement teleportation activation logic using XRRayInteractor and InputActionProperty.

---
+++ unity/scripts/TeleportationActivator.cs
@@ -0,0 +1,28 @@
+/// TeleportationActivator.cs
+
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.XR.Interaction.Toolkit.Interactors;
+
+public class TeleportationActivator : MonoBehaviour
+{
+    public  XRRayInteractor        teleportInteractor; /// ray
+    public  InputActionProperty  teleportActivatorAction;  /// input 이벤트 발생 시 action과 연결 위한 변수
+
+    void Start()
+    {
+        teleportInteractor.gameObject.SetActive(false);
+        teleportActivatorAction.action.performed += Action_performed;  /// 버튼 클릭 이벤트 시 함수 호출
+    }
+
+    private void Action_performed(InputAction.CallbackContext obj)
+    {
+        teleportInteractor.gameObject.SetActive(true);
+    }
+
+    void Update()
+    {
+        if(teleportActivatorAction.action.WasReleasedThisFrame())
+            teleportInteractor.gameObject.SetActive(false);
+    }
+}
