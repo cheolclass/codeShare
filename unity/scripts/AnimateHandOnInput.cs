9686bd164085c715a51f2f0d704fee075729dbe1
cheolclass <cheolclass@gmail.com>
2025-12-11 19:14:57 +0900

    Add AnimateHandOnInput script for hand animation

---
+++ unity/scripts/AnimateHandOnInput.cs
@@ -0,0 +1,26 @@
+/// AnimateHandOnInput.cs
+
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+
+public class AnimateHandOnInput : MonoBehaviour
+{
+    public  InputActionProperty     gripActionValue; 
+    public  InputActionProperty     triggerActionValue;
+
+    public Animator handAnimator;
+
+    void Start()
+    {        
+    }
+
+    void Update()
+    {
+        float gripValue = gripActionValue.action.ReadValue<float>();
+        float triggerValue = triggerActionValue.action.ReadValue<float>();
+
+        handAnimator.SetFloat("Grip", gripValue);
+        handAnimator.SetFloat("Trigger", triggerValue);
+    }
+}
