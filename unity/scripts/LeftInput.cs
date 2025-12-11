5a968984748befe6e641f1ced6e3be64b732f2c4
cheolclass <cheolclass@gmail.com>
2025-12-11 19:06:56 +0900

    Add LeftInput script for handling input actions

---
+++ unity/scripts/LeftInput.cs
@@ -0,0 +1,26 @@
+/// LeftInput.cs
+
+
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class LeftInput : MonoBehaviour
+{
+    public InputActionProperty myActionValue;
+    public InputActionProperty myActionButton;
+
+    void Start()
+    {
+    }
+
+    void Update()
+    {
+        float value = myActionValue.action.ReadValue<float>();
+        if(value > 0f)
+            Debug.Log("Left Grip Button Value: " + value);
+
+        bool buttonPressed = myActionButton.action.IsPressed();
+        if (buttonPressed)
+            Debug.Log("Left Grip Button Pressed: " + buttonPressed);
+    }
+}
