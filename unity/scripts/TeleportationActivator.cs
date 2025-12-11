233da74b2f3c0a8ee9e78c982a2254d921192325
cheolclass <cheolclass@gmail.com>
2025-12-11 23:25:05 +0900

    Refactor TeleportationActivator for clarity

--- unity/scripts/TeleportationActivator.cs
+++ unity/scripts/TeleportationActivator.cs
@@ -6,13 +6,14 @@ using UnityEngine.XR.Interaction.Toolkit.Interactors;
 
 public class TeleportationActivator : MonoBehaviour
 {
-    public  XRRayInteractor        teleportInteractor; /// ray
-    public  InputActionProperty  teleportActivatorAction;  /// input 이벤트 발생 시 action과 연결 위한 변수
+    public XRRayInteractor teleportInteractor;  /// ray
+    public InputActionProperty teleportActivatorAction;  /// input 이벤트 발생 시 action과 연결 위한 변수
 
+    /// 오류 발생 가능
     void Start()
     {
         teleportInteractor.gameObject.SetActive(false);
-        teleportActivatorAction.action.performed += Action_performed;  /// 버튼 클릭 이벤트 시 함수 호출
+        teleportActivatorAction.action.performed += Action_performed; /// 버튼 클릭 이벤트 시 함수 호출
     }
 
     private void Action_performed(InputAction.CallbackContext obj)
@@ -22,7 +23,7 @@ public class TeleportationActivator : MonoBehaviour
 
     void Update()
     {
-        if(teleportActivatorAction.action.WasReleasedThisFrame())
+        if (teleportActivatorAction.action.WasReleasedThisFrame())  /// 사용자가 버튼을 놓았는지 체크 
             teleportInteractor.gameObject.SetActive(false);
     }
 }
