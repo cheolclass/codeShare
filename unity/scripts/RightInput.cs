421c2be56d4e219aba909c0bf496a47f0a0a232d
cheolclass <cheolclass@gmail.com>
2025-12-11 19:12:48 +0900

    Add RightInput script for handling input actions

---
+++ unity/scripts/RightInput.cs
@@ -0,0 +1,25 @@
+/// RightInput.cs
+
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class RightInput : MonoBehaviour
+{
+    public InputActionProperty myActionValue;
+    public InputActionProperty myActionButton;
+
+    void Start()
+    {
+    }
+
+    void Update()
+    {
+        float value = myActionValue.action.ReadValue<float>();
+        if (value > 0f) 
+            Debug.Log("Right Activate Button Value: " + value);
+
+        bool buttonPressed = myActionButton.action.IsPressed();
+        if (buttonPressed)
+            Debug.Log("Right Activate Button Pressed: " + buttonPressed);
+    }
+}
