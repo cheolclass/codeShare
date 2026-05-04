4b6ffb65cae6e02f2f151a544a5ade571fe62b1b
cheolclass <cheolclass@gmail.com>
2026-05-04 12:54:14 +0900

    Update PlayerController2.cs

--- unity/scripts/memoryPool/PlayerController2.cs
+++ unity/scripts/memoryPool/PlayerController2.cs
@@ -5,13 +5,13 @@ public class PlayerController2 : MonoBehaviour
 {
 	[SerializeField]
 	private GameObject projectilePrefab;
-	private MemoryPool	memoryPool;  ///
+	private MemoryPool memoryPool;  ///
 
 	private float angle = 0f;
 
 	private void Awake()  ///
 	{
-		memoryPool = new MemoryPool(projectilePrefab); 
+		memoryPool = new MemoryPool(projectilePrefab);
 	}
 
 	private void OnApplicationQuit()  /// 
@@ -19,23 +19,23 @@ public class PlayerController2 : MonoBehaviour
 		Debug.Log("Destroy all objects");
 		memoryPool.DestroyObjects();
 	}
-	
+
 	private void Update()
 	{
-		//if (Input.GetKeyDown(KeyCode.Space))   /// 연사 방지
+		///if (Input.GetKey(KeyCode.Space))   /// 연사  
 		if (Keyboard.current.spaceKey.wasPressedThisFrame)  /// 간단한 새로운 입력 시스템을 이용 
 		{
-			GameObject	clone = memoryPool.ActivatePoolItem();
-			clone.transform.position = transform.position;			
-			
+			GameObject clone = memoryPool.ActivatePoolItem();
+			clone.transform.position = transform.position;
+
 			// 발사 방향 계산 (degree => radian)
-			float rad = angle * Mathf.Deg2Rad;  
+			float rad = angle * Mathf.Deg2Rad;
 			float x = Mathf.Cos(rad);
 			float y = Mathf.Sin(rad);
 
 			// 발사체 방향 설정
 			///clone.GetComponent<Projectile>()?.Setup(new Vector2(x, y));
-			clone.GetComponent<Projectile>()?.Setup(new Vector2(x, y), memoryPool);  /// 
+			clone.GetComponent<Projectile2>()?.Setup(new Vector2(x, y), memoryPool);  /// 
 
 			// 다음 발사 각도 증가 (5도씩 회전하며 발사)
 			angle += 5f;
