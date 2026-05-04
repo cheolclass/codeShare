4323606afb5c88f62deb0ea0a75172fe33707112
cheolclass <cheolclass@gmail.com>
2026-05-04 11:57:08 +0900

    Update Projectile2.cs

--- unity/scripts/memoryPool/Projectile2.cs
+++ unity/scripts/memoryPool/Projectile2.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 public class Projectile : MonoBehaviour
 {
 	private Movement2D movement2D;
+	private MemoryPool	memoryPool;  ///
 	private Vector2 limitMin = new Vector2(-8, -5);
 	private Vector2 limitMax = new Vector2(8, 5);
 
@@ -11,8 +12,9 @@ public class Projectile : MonoBehaviour
 		movement2D = GetComponent<Movement2D>();
 	}
 
-	public void Setup(Vector3 direction)
+	public void Setup(Vector3 direction, MemoryPool	memoryPool)
 	{
+		this.memoryPool = memoryPool;  ///
 		movement2D.MoveTo(direction);
 	}
 
@@ -22,14 +24,16 @@ public class Projectile : MonoBehaviour
 		if (transform.position.x < limitMin.x || transform.position.x > limitMax.x)
 		{
 			// 발사체 오브젝트 삭제
-			Destroy(gameObject);
+			///Destroy(gameObject);
+			memoryPool.DeactivatePoolItem(gameObject);  ///
 		}
 
 		// 게임 오브젝트의 y좌표가 화면 바깥으로 나가면
 		if (transform.position.y < limitMin.y || transform.position.y > limitMax.y)
 		{
 			// 발사체 오브젝트 삭제
-			Destroy(gameObject);
+			///Destroy(gameObject);
+			memoryPool.DeactivatePoolItem(gameObject);   ///
 		}
 	}
 }
