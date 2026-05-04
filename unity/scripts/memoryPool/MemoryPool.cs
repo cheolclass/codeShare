2b4c3a6df3fea49fbe9dcdcd3bda08b38c6ad5f3
cheolclass <cheolclass@gmail.com>
2026-05-04 15:11:47 +0900

    Update MemoryPool.cs

--- unity/scripts/memoryPool/MemoryPool.cs
+++ unity/scripts/memoryPool/MemoryPool.cs
@@ -6,10 +6,10 @@ public class MemoryPool
 	// 메모리 풀로 관리되는 오브젝트 정보
 	private class PoolItem
 	{
-		public	GameObject	gameObject;		// 화면에 보이는 실제 게임오브젝트
-		private	bool		isActive;		// "gameObject"의 활성화/비활성화 정보
+		public GameObject gameObject;       // 화면에 보이는 실제 게임오브젝트
+		private bool isActive;      // "gameObject"의 활성화/비활성화 정보
 
-		public	bool		IsActive
+		public bool IsActive
 		{
 			set
 			{
@@ -20,23 +20,23 @@ public class MemoryPool
 		}
 	}
 
-	private	int	increaseCount = 5;			// 오브젝트가 부족할 때 Instantiate()로 추가 생성되는 오브젝트 개수
-	private	int	maxCount;					// 현재 리스트에 등록되어 있는 오브젝트 개수
-	private	int	activeCount;				// 현재 게임에 사용되고 있는(활성화) 오브젝트 개수
+	private int increaseCount = 5;          // 오브젝트가 부족할 때 Instantiate()로 추가 생성되는 오브젝트 개수
+	private int maxCount;                   // 현재 리스트에 등록되어 있는 오브젝트 개수
+	private int activeCount;                // 현재 게임에 사용되고 있는(활성화) 오브젝트 개수
 
-	private	GameObject		poolObject;		// 오브젝트 풀링에서 관리하는 게임 오브젝트 프리팹
-	private	List<PoolItem>	poolItemList;	// 관리되는 모든 오브젝트를 저장하는 리스트
+	private GameObject poolObject;      // 오브젝트 풀링에서 관리하는 게임 오브젝트 프리팹
+	private List<PoolItem> poolItemList;    // 관리되는 모든 오브젝트를 저장하는 리스트
 
 	// 오브젝트가 임시로 보관되는 위치
 	///private Vector3 tempPosition = new Vector3(float.MaxValue, float.MaxValue, float.MaxValue);
-	
+
 	public MemoryPool(GameObject poolObject)
 	{
-		maxCount		= 0;
-		activeCount		= 0;
-		this.poolObject	= poolObject;
+		maxCount = 0;
+		activeCount = 0;
+		this.poolObject = poolObject;
 
-		poolItemList	= new List<PoolItem>();
+		poolItemList = new List<PoolItem>();
 
 		InstantiateObjects();
 	}
@@ -45,7 +45,7 @@ public class MemoryPool
 	{
 		maxCount += increaseCount;
 
-		for ( int i = 0; i < increaseCount; ++ i )
+		for (int i = 0; i < increaseCount; ++i)
 		{
 			PoolItem poolItem = new PoolItem();
 
@@ -56,13 +56,13 @@ public class MemoryPool
 			poolItemList.Add(poolItem);
 		}
 	}
-	
+
 	public void DestroyObjects()
 	{
-		if ( poolItemList == null ) return;
+		if (poolItemList == null) return;
 
 		int count = poolItemList.Count;
-		for ( int i = 0; i < count; ++ i )
+		for (int i = 0; i < count; ++i)
 		{
 			GameObject.Destroy(poolItemList[i].gameObject);
 		}
@@ -72,26 +72,26 @@ public class MemoryPool
 
 	public GameObject ActivatePoolItem() ///Vector3 position)
 	{
-		if ( poolItemList == null ) return null;
+		if (poolItemList == null) return null;
 
 		// 현재 생성해서 관리하는 모든 오브젝트 개수와 현재 활성화 상태인 오브젝트 개수 비교
 		// 모든 오브젝트가 활성화 상태이면 새로운 오브젝트 필요
-		if ( maxCount == activeCount )
+		if (maxCount == activeCount)
 		{
 			InstantiateObjects();
 		}
 
 		int count = poolItemList.Count;
-		for ( int i = 0; i < count; ++ i )
+		for (int i = 0; i < count; ++i)
 		{
 			PoolItem poolItem = poolItemList[i];
 
-			if ( poolItem.IsActive == false )
+			if (poolItem.IsActive == false)
 			{
-				activeCount ++;
+				activeCount++;
 
 				///poolItem.gameObject.transform.position	= position;
-				poolItem.IsActive	= true;
+				poolItem.IsActive = true;
 
 				return poolItem.gameObject;
 			}
@@ -102,16 +102,16 @@ public class MemoryPool
 
 	public void DeactivatePoolItem(GameObject removeObject)
 	{
-		if ( poolItemList == null || removeObject == null ) return;
+		if (poolItemList == null || removeObject == null) return;
 
 		int count = poolItemList.Count;
-		for ( int i = 0; i < count; ++ i )
+		for (int i = 0; i < count; ++i)
 		{
 			PoolItem poolItem = poolItemList[i];
 
-			if ( poolItem.gameObject == removeObject )
+			if (poolItem.gameObject == removeObject)
 			{
-				activeCount --;
+				activeCount--;
 
 				poolItem.IsActive = false;
 				///poolItem.gameObject.transform.position	= tempPosition;
@@ -123,14 +123,14 @@ public class MemoryPool
 
 	public void DeactivateAllPoolItems()
 	{
-		if ( poolItemList == null ) return;
+		if (poolItemList == null) return;
 
 		int count = poolItemList.Count;
-		for ( int i = 0; i < count; ++ i )
+		for (int i = 0; i < count; ++i)
 		{
 			PoolItem poolItem = poolItemList[i];
 
-			if ( poolItem.gameObject != null && poolItem.IsActive == true )
+			if (poolItem.gameObject != null && poolItem.IsActive == true)
 			{
 				poolItem.IsActive = false;
 				///poolItem.gameObject.transform.position	= tempPosition;
