49d6f6a61e8fe82bf5e829e65b739815087e0b0c
cheolclass <cheolclass@gmail.com>
2026-05-04 11:53:23 +0900

    Create Projectile2.cs

---
+++ unity/scripts/memoryPool/Projectile2.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+	private Movement2D movement2D;
+	private Vector2 limitMin = new Vector2(-8, -5);
+	private Vector2 limitMax = new Vector2(8, 5);
+
+	private void Awake()
+	{
+		movement2D = GetComponent<Movement2D>();
+	}
+
+	public void Setup(Vector3 direction)
+	{
+		movement2D.MoveTo(direction);
+	}
+
+	private void Update()
+	{
+		// 게임 오브젝트의 x좌표가 화면 바깥으로 나가면
+		if (transform.position.x < limitMin.x || transform.position.x > limitMax.x)
+		{
+			// 발사체 오브젝트 삭제
+			Destroy(gameObject);
+		}
+
+		// 게임 오브젝트의 y좌표가 화면 바깥으로 나가면
+		if (transform.position.y < limitMin.y || transform.position.y > limitMax.y)
+		{
+			// 발사체 오브젝트 삭제
+			Destroy(gameObject);
+		}
+	}
+}
