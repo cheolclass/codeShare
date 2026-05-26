d2aea47dc8f9b3e8b938056fa24aecc626b8fbcc
cheolclass <cheolclass@gmail.com>
2026-05-26 17:25:21 +0900

    Update Bullet Controller.cs

--- unity/scripts/2dDodgeGame/InputHandling/Bullet Controller.cs
+++ unity/scripts/2dDodgeGame/InputHandling/Bullet Controller.cs
@@ -3,20 +3,20 @@ using UnityEngine;
 public class BulletController: MonoBehaviour
 {
 	public float bulletVerticalSpeed = 15f; // 위로 날아가는 힘
-	private Rigidbody2D rb;
+	private Rigidbody2D mRB;
 
 	void Awake()
 	{
-		rb = GetComponent<Rigidbody2D>();
+		mRB = GetComponent<Rigidbody2D>();
 	}
 
 	// 플레이어의 속도를 외부에서 넣어주는 함수
 	public void SetInertia(Vector2 playerVelocity)
 	{
-		if (rb == null) rb = GetComponent<Rigidbody2D>();
+		if (mRB == null) mRB = GetComponent<Rigidbody2D>();
 
 		// (플레이어의 좌우 속도) + (총알 본래의 수직 속도)
-		rb.linearVelocity = new Vector2(playerVelocity.x, bulletVerticalSpeed);
+		mRB.linearVelocity = new Vector2(playerVelocity.x, bulletVerticalSpeed);
 	}
 
 	void Start()
